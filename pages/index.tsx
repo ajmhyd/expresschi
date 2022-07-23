a08959a9682f7a7995fdd8194fe66a836da62150
Alex Matseshe <amatseshe@gmail.com>
2022-07-22 22:50:33 -0500

    💄 add margin right to minutes

--- pages/index.tsx
+++ pages/index.tsx
@@ -49,56 +49,56 @@ export default function IndexPage({ data }: IndexPageProps) {
                 <span className='block text-blue-300 xl:inline'>Unknown</span>
               </h1>
             ) : (
-                <dl className='mt-5 grid grid-cols-1 rounded-lg bg-white dark:bg-gray-900 overflow-hidden shadow divide-y divide-gray-200 md:grid-cols-3 md:divide-y-0 md:divide-x'>
-                  <div>
-                    <div className='px-4 py-5 sm:p-6'>
-                      <dt className='text-base font-normal  text-gray-900 dark:text-white'>
-                        Direction
+              <dl className='mt-5 grid grid-cols-1 rounded-lg bg-white dark:bg-gray-900 overflow-hidden shadow divide-y divide-gray-200 md:grid-cols-3 md:divide-y-0 md:divide-x'>
+                <div>
+                  <div className='px-4 py-5 sm:p-6'>
+                    <dt className='text-base font-normal  text-gray-900 dark:text-white'>
+                      Direction
                     </dt>
-                      <dd className='mt-1 flex justify-between items-baseline md:block lg:flex'>
-                        <div className='flex items-baseline text-2xl font-semibold text-blue-300'>
-                          {data.direction}
-                        </div>
-                      </dd>
-                    </div>
-                  </div>
-
-                  <div>
-                    <div className='px-4 py-5 sm:p-6'>
-                      <dt className='text-base font-normal  text-gray-900 dark:text-white'>
-                        Travel Time
-                    </dt>
-                      <dd className='mt-1 flex justify-between items-baseline md:block lg:flex'>
-                        <div className='flex items-baseline text-2xl font-semibold text-blue-300'>
-                          {data.travelTime} minutes
-                        <span className='ml-2 text-sm font-medium text-gray-500'>
-                            {data.averageTravelTime} min avg
-                        </span>
-                        </div>
-
-                        <TravelTimeDifference
-                          travelTime={+data.travelTime}
-                          averageTravelTime={+data.averageTravelTime}
-                        />
-                      </dd>
-                    </div>
-                  </div>
-
-                  <div>
-                    <div className='px-4 py-5 sm:p-6'>
-                      <dt className='text-base font-normal text-gray-900 dark:text-white'>
-                        Speed
-                    </dt>
-                      <dd className='mt-1 flex justify-between items-baseline md:block lg:flex'>
-                        <div className='flex items-baseline text-2xl font-semibold text-blue-300'>
-                          {data.speed} mph
+                    <dd className='mt-1 flex justify-between items-baseline md:block lg:flex'>
+                      <div className='flex items-baseline text-2xl font-semibold text-blue-300'>
+                        {data.direction}
                       </div>
-                        <Congestion speed={+data.speed} />
-                      </dd>
-                    </div>
+                    </dd>
                   </div>
-                </dl>
-              )}
+                </div>
+
+                <div>
+                  <div className='px-4 py-5 sm:p-6'>
+                    <dt className='text-base font-normal text-gray-900 dark:text-white'>
+                      Travel Time
+                    </dt>
+                    <dd className='mt-1 flex justify-between items-baseline md:block lg:flex'>
+                      <div className='flex items-baseline text-2xl mr-2 font-semibold text-blue-300'>
+                        {data.travelTime} minutes
+                        <span className='ml-2 text-sm font-medium text-gray-500'>
+                          {data.averageTravelTime} min avg
+                        </span>
+                      </div>
+
+                      <TravelTimeDifference
+                        travelTime={+data.travelTime}
+                        averageTravelTime={+data.averageTravelTime}
+                      />
+                    </dd>
+                  </div>
+                </div>
+
+                <div>
+                  <div className='px-4 py-5 sm:p-6'>
+                    <dt className='text-base font-normal text-gray-900 dark:text-white'>
+                      Speed
+                    </dt>
+                    <dd className='mt-1 flex justify-between items-baseline md:block lg:flex'>
+                      <div className='flex items-baseline text-2xl font-semibold text-blue-300'>
+                        {data.speed} mph
+                      </div>
+                      <Congestion speed={+data.speed} />
+                    </dd>
+                  </div>
+                </div>
+              </dl>
+            )}
           </div>
         </main>
         <Footer />
@@ -164,7 +164,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
     }
 
     data = { updatedAt, direction, travelTime, averageTravelTime, speed };
-
   } catch (error) {
     throw error;
   } finally {
